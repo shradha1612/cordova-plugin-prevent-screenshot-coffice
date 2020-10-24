6fa530b9a7d4c8744e6d2e4b0e6b2b7a752275d8
raphaelcr93 <raphaelcr93@hotmail.com>
2020-10-23 21:27:42 -0300

    Added listening function for android.
    used two stack overflow topics:
    //https://stackoverflow.com/questions/31360296/listen-for-screenshot-action-in-android it shows how to listen for screenshot event
    //https://stackoverflow.com/questions/54939027/triggering-javascript-event-from-android how to trigger an event from android to JS

--- src/android/com/coffice/ScreenshotBlocker.java
+++ src/android/com/coffice/ScreenshotBlocker.java
@@ -10,16 +10,27 @@ import org.json.JSONException;
 public class ScreenshotBlocker extends CordovaPlugin{
     private com.coffice.ScreenshotBlocker mContext;
 
+    private static final String TAG = "ScreenshotBlocker";
+    static ScreenshotBlocker instance = null;
+    static CordovaWebView cordovaWebView;
+    static CordovaInterface cordovaInterface;
+
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         Activity activity = this.cordova.getActivity();
         activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
+        instance = this;
+        cordovaWebView = webView;
+        cordovaInterface = cordova;
     }
 
     @Override
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
         mContext = this;
+
         if (action.equals("enable")) {
             mContext.cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
@@ -59,5 +70,152 @@ public class ScreenshotBlocker extends CordovaPlugin{
         }
 
     }
+    
+    
+    private ScreenShotContentObserver screenShotContentObserver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HandlerThread handlerThread = new HandlerThread("content_observer");
+        handlerThread.start();
+        final Handler handler = new Handler(handlerThread.getLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
+        screenShotContentObserver = new ScreenShotContentObserver(handler, this) {
+            @Override
+            protected void onScreenShot(String path, String fileName) {
+                //File file = new File(path); //this is the file of screenshot image
+                triggerJavascriptEvent("onTookScreenshot");
+            }
+        };
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getContentResolver().registerContentObserver(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                true,
+                screenShotContentObserver
+        );
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        try {
+            getContentResolver().unregisterContentObserver(screenShotContentObserver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        instance = null;
+        try {
+            getContentResolver().unregisterContentObserver(screenShotContentObserver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //https://stackoverflow.com/questions/54939027/triggering-javascript-event-from-android how to trigger an event from android to JS
+    private static void executeGlobalJavascript(final String jsString) {
+        if (instance == null) {return;}
+        instance.cordovaInterface.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    instance.cordovaWebView.loadUrl("javascript:" + jsString);
+                } catch (Exception e) {
+                    Log.e(TAG, "Error executing javascript: "+ e.toString());
+                }
+            }
+        });
+    }
+
+    public static void triggerJavascriptEvent(final String eventName){
+        executeGlobalJavascript(String.format("document.dispatchEvent(new Event('%s'));", eventName));
+    }
+
+}
+
+}
+
+
+
+//Got from https://stackoverflow.com/questions/31360296/listen-for-screenshot-action-in-android it shows how to listen for screenshot event
+public abstract class ScreenShotContentObserver extends ContentObserver {
+
+    private Context context;
+    private boolean isFromEdit = false;
+    private String previousPath;
+
+    public ScreenShotContentObserver(Handler handler, Context context) {
+        super(handler);
+        this.context = context;
+    }
+
+    @Override
+    public boolean deliverSelfNotifications() {
+        return super.deliverSelfNotifications();
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        super.onChange(selfChange);
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(uri, new String[]{
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.DATA
+            }, null, null, null);
+            if (cursor != null && cursor.moveToLast()) {
+                int displayNameColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME);
+                int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                String fileName = cursor.getString(displayNameColumnIndex);
+                String path = cursor.getString(dataColumnIndex);
+                if (new File(path).lastModified() >= System.currentTimeMillis() - 10000) {
+                    if (isScreenshot(path) && !isFromEdit && !(previousPath != null && previousPath.equals(path))) {
+                        onScreenShot(path, fileName);
+                    }
+                    previousPath = path;
+                    isFromEdit = false;
+                } else {
+                    cursor.close();
+                    return;
+                }
+            }
+        } catch (Throwable t) {
+            isFromEdit = true;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        super.onChange(selfChange, uri);
+    }
+
+    private boolean isScreenshot(String path) {
+        return path != null && path.toLowerCase().contains("screenshot");
+    }
+
+    protected abstract void onScreenShot(String path, String fileName);
 
 }
