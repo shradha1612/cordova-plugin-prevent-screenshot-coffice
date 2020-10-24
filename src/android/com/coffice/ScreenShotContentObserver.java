66411074bedfebf43a9ffc3a1ef0e46f51d57479
raphaelcr93 <raphaelcr93@hotmail.com>
2020-10-23 21:37:35 -0300

    Added file ScreenShotContentObserver
    Got from https://stackoverflow.com/questions/31360296/listen-for-screenshot-action-in-android it shows how to listen for screenshot event

---
+++ src/android/com/coffice/ScreenShotContentObserver.java
@@ -0,0 +1,63 @@
+public abstract class ScreenShotContentObserver extends ContentObserver {
+//Got from https://stackoverflow.com/questions/31360296/listen-for-screenshot-action-in-android it shows how to listen for screenshot event
+
+    private Context context;
+    private boolean isFromEdit = false;
+    private String previousPath;
+
+    public ScreenShotContentObserver(Handler handler, Context context) {
+        super(handler);
+        this.context = context;
+    }
+
+    @Override
+    public boolean deliverSelfNotifications() {
+        return super.deliverSelfNotifications();
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        super.onChange(selfChange);
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(uri, new String[]{
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    MediaStore.Images.Media.DATA
+            }, null, null, null);
+            if (cursor != null && cursor.moveToLast()) {
+                int displayNameColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME);
+                int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                String fileName = cursor.getString(displayNameColumnIndex);
+                String path = cursor.getString(dataColumnIndex);
+                if (new File(path).lastModified() >= System.currentTimeMillis() - 10000) {
+                    if (isScreenshot(path) && !isFromEdit && !(previousPath != null && previousPath.equals(path))) {
+                        onScreenShot(path, fileName);
+                    }
+                    previousPath = path;
+                    isFromEdit = false;
+                } else {
+                    cursor.close();
+                    return;
+                }
+            }
+        } catch (Throwable t) {
+            isFromEdit = true;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        super.onChange(selfChange, uri);
+    }
+
+    private boolean isScreenshot(String path) {
+        return path != null && path.toLowerCase().contains("screenshot");
+    }
+
+    protected abstract void onScreenShot(String path, String fileName);
+
+}
